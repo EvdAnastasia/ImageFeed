2fd4ed928b2d60037325bfe917725eb8717b7b81
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2024-09-09 23:51:46 +0300

    Добавлена таблица для ленты и создана кастомная ячейка

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Anastasiia on 09.09.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
