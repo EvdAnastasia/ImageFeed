bada95c609da6625e56a5c3ca1c19c69a2ce2d9f
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2024-09-12 21:42:17 +0300

    Свёрстан главный экран с лентой изображений

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -10,4 +10,23 @@ import UIKit
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
     
+    @IBOutlet private var cellImage: UIImageView!
+    @IBOutlet private var dateLabelView: UIView!
+    @IBOutlet private var dateLabel: UILabel!
+    @IBOutlet private var likeButton: UIButton!
+    
+    func configCell(image: UIImage, date: String, isLiked: Bool) {
+        cellImage.image = image
+        dateLabel.text = date
+        
+        let likeImage = isLiked ? UIImage(named: "LikeButtonOn") : UIImage(named: "LikeButtonOff")
+        likeButton.setImage(likeImage, for: .normal)
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        let colorTop =  UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 0).cgColor
+        let colorBottom = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 0.2).cgColor
+        gradientLayer.frame.size = dateLabelView.frame.size
+        gradientLayer.colors = [colorTop, colorBottom]
+        dateLabelView.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
