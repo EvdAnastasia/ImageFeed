b7933ade471ef6e07801e344880e12edd7076e68
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2024-09-17 16:31:56 +0300

    Реализован показ фото с помощью Navigation Controller

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Anastasiia on 17.09.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+    
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return } // 1
+            imageView.image = image // 2
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
